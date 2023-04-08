a3d0a01ec06e9ea791578904e716bda1cb4d3ad2
Amith-B <37371517+Amith-B@users.noreply.github.com>
2023-04-08 14:51:45 +0000

    fix(selectors): remove ng as prefix from directive selectors

--- projects/ng-util-directive/src/lib/intersection/stories/Intersection.stories.ts
+++ projects/ng-util-directive/src/lib/intersection/stories/Intersection.stories.ts
@@ -11,11 +11,11 @@ export default {
     }),
   ],
   argTypes: {
-    ngContainerIntersection: {
+    containerIntersection: {
       description:
         'An output event which emits `IntersectionObserverEntry` for the the dom element on which this directive is used.<br/>This directive uses `IntersectionObserver` internally and disconnects the observer when view is destroyed',
     },
-    ngContainerIntersectionOptions: {
+    containerIntersectionOptions: {
       description:
         'Options for `IntersectionObserver`. <br/>`{ root?: Element | Document | null; rootMargin?: string; threshold?: number | number[]; }`.<br/>[See Mozilla docs for more details](https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/IntersectionObserver). <br/> <b>Note: `threshold` and `rootMargin` options works well when `root` option is passed. With storybook control you cannot use `root` as it expects `Element | Document` </b>',
       control: 'object',
@@ -50,8 +50,8 @@ export const Basic: Story = (args: any) => ({
     <p #info style="position: sticky; top: 0; margin: 0; margin-left: 150px; padding: 0.5rem;"></p>
     <div style="height: 2000px; padding-top: 800px;">
       <img
-        (ngContainerIntersection)="handleIntersection($event, info)"
-        [ngContainerIntersectionOptions]="ngContainerIntersectionOptions"
+        (containerIntersection)="handleIntersection($event, info)"
+        [containerIntersectionOptions]="containerIntersectionOptions"
         style="width: 200px; height: 100px; border-radius: 20px;"
         src="art.jpg"
       />
@@ -78,7 +78,7 @@ export const Basic: Story = (args: any) => ({
 
 Basic.args = {
   console: console,
-  ngContainerIntersectionOptions: {},
+  containerIntersectionOptions: {},
 };
 
 export const WithOptions: Story = (args: any) => ({
@@ -109,8 +109,8 @@ export const WithOptions: Story = (args: any) => ({
     <p #info style="position: sticky; top: 0; margin: 0; margin-left: 150px; padding: 0.5rem;"></p>
     <div style="height: 2000px; padding-top: 800px;">
       <img
-        (ngContainerIntersection)="handleIntersection($event, info)"
-        [ngContainerIntersectionOptions]="{threshold: 1, root: rootEl}"
+        (containerIntersection)="handleIntersection($event, info)"
+        [containerIntersectionOptions]="{threshold: 1, root: rootEl}"
         style="width: 200px; height: 100px; border-radius: 20px;"
         src="art.jpg"
       />
@@ -123,8 +123,8 @@ export const WithOptions: Story = (args: any) => ({
     <p #infoRootMargin style="position: sticky; top: 0; margin: 0; margin-left: 150px; padding: 0.5rem;"></p>
     <div style="height: 2000px; padding-top: 800px;">
       <img
-        (ngContainerIntersection)="handleIntersection($event, infoRootMargin, true)"
-        [ngContainerIntersectionOptions]="{rootMargin: '200px', root: rootElrootMargin}"
+        (containerIntersection)="handleIntersection($event, infoRootMargin, true)"
+        [containerIntersectionOptions]="{rootMargin: '200px', root: rootElrootMargin}"
         style="width: 200px; height: 100px; border-radius: 20px;"
         src="art.jpg"
       />
@@ -154,11 +154,11 @@ export const WithOptions: Story = (args: any) => ({
 
 WithOptions.args = {
   console: console,
-  ngContainerIntersectionOptions: undefined,
+  containerIntersectionOptions: undefined,
 };
 
-WithOptions.argTypes={
-  ngContainerIntersectionOptions: {
-    control: false
-  }
-}
+WithOptions.argTypes = {
+  containerIntersectionOptions: {
+    control: false,
+  },
+};
