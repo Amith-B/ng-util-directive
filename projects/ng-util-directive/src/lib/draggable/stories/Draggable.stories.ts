326bdfee3a73575c8f503d517e60ba314d4b49be
Amith-B <37371517+Amith-B@users.noreply.github.com>
2023-04-11 07:28:06 +0000

    feat(draggable): add teleportto prop to directive

--- projects/ng-util-directive/src/lib/draggable/stories/Draggable.stories.ts
+++ projects/ng-util-directive/src/lib/draggable/stories/Draggable.stories.ts
@@ -14,6 +14,7 @@ export default {
     draggableSticky: {
       description:
         'When `true` the draggable element will move to either right or left of the screen when pointer leaves the host',
+      defaultValue: { summary: 'true' },
     },
     draggableStickyMargins: {
       description:
@@ -24,19 +25,25 @@ export default {
       description: 'Can be used to change the sticky transition',
       defaultValue: { summary: 'left 0.3s ease' },
     },
+    draggableTeleportTo: {
+      description:
+        'A css id selector to which the host element will be appended when dragging starts. If `undefined` nothing happens, if some css selector is passed then the host element will be appended to the specified css selector element if already available in dom or else it will create one with give selector and will append it to body',
+      defaultValue: { summary: 'undefined' },
+    },
   },
 } as Meta;
 
 export const Basic: Story = (args: any) => ({
   props: args,
   template: `
-  <b>Note: The dom element on which you use this directive gets a style position: fixed; when dragged, if any of the parent of this dom element has transform css property in it then the position wouldn't behave as expected, so it is recommended to keep your floating item out of the parent which has transform css property in it</b>
+  <b>Note:</b> The dom element on which you use this directive gets a style <b>position: fixed;</b> when dragged, if any of the parent of this dom element has transform css property in it then the position wouldn't behave as expected, so it is recommended to keep your floating item out of the parent which has transform css property in it. Or alternatively you can use <b>draggableTeleportTo</b> prop to move it under any specified dom when dragging starts.
   <br/>
   <div
     draggable
     [draggableSticky]="draggableSticky"
     [draggableStickyMargins]="draggableStickyMargins"
     [draggableStickyTransition]="draggableStickyTransition"
+    [draggableTeleportTo]="draggableTeleportTo"
     class="floating-button"
   >+</div>
   `,
@@ -61,4 +68,5 @@ Basic.args = {
   draggableSticky: true,
   draggableStickyMargins: '0px',
   draggableStickyTransition: 'left 0.3s ease',
+  draggableTeleportTo: 'modal-container',
 };
